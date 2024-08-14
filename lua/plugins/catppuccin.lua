18c0ec3e72e63edd960e6dd887941225bb0a41e3
chaumage <chaumatg@gmail.com>
2024-08-14 18:33:23 +0200

    Config de catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,1 +1,11 @@
-retunr { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  flavour = "mocha",
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
+  end
+}
+
